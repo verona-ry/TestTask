61593e964e531e80999f61c761f752875ab3f16a
verona-ry <100212248+verona-ry@users.noreply.github.com>
2022-02-22 15:53:07 +0200

    Initial commit

---
+++ LuxoftTestTask_VeronikaRybka/TestTask.cs
@@ -0,0 +1,41 @@
+﻿namespace LuxoftTestTask_VeronikaRybka
+{
+    public class TestTask
+    {
+        public IEnumerable<int> GetDigits(int source)
+        {
+            while (source > 0)
+            {
+                var digit = source % 10;
+                source /= 10;
+                yield return digit;
+            }
+        }
+        public int GetDigitSum(int number)
+        {
+            IEnumerable<int> digits = GetDigits(number);
+            int resNum = 0;
+            foreach (int digit in digits)
+            {
+                resNum += digit;
+            }
+            return resNum;
+        }
+        public List<int> PerformTask(int min, int max, int sum)
+        {
+            if (!(min > max) && !(min < 0) && !(max < 0) && !(sum < 0))
+            {
+                List<int> nums = new List<int>();
+                for (int i = 0; i < (max - min + 1); i++)
+                {
+                    if (sum.Equals(GetDigitSum(min + i)))
+                    {
+                        nums.Add(min + i);
+                    }
+                }
+                return nums;
+            }
+            return null;
+        }
+    }
+}
