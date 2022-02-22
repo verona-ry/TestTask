9e1bade6333cf21a5cd54236b379bad6ba37668f
verona-ry <100212248+verona-ry@users.noreply.github.com>
2022-02-22 15:59:37 +0200

    Add comments

--- LuxoftTestTask_VeronikaRybka/TestTask.cs
+++ LuxoftTestTask_VeronikaRybka/TestTask.cs
@@ -2,7 +2,8 @@
 {
     public class TestTask
     {
-        public IEnumerable<int> GetDigits(int source)
+        //Gets each digit of the number
+        private IEnumerable<int> GetDigits(int source)
         {
             while (source > 0)
             {
@@ -11,7 +12,9 @@
                 yield return digit;
             }
         }
-        public int GetDigitSum(int number)
+
+        //Adds all digits of the number to find their sum
+        private int GetDigitSum(int number)
         {
             IEnumerable<int> digits = GetDigits(number);
             int resNum = 0;
@@ -21,6 +24,8 @@
             }
             return resNum;
         }
+
+        //Finds all the numbers which digit sum equals to the input parameter "sum"
         public List<int> PerformTask(int min, int max, int sum)
         {
             if (!(min > max) && !(min < 0) && !(max < 0) && !(sum < 0))
